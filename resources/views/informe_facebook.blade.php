faf8556490cb28cc787e67bc4aafdea365547f02
SERGIO ANDIA <sergioandia11@gmail.com>
2024-08-25 21:19:16 -0400

    cambios en el pdf falta ajustar y agregar clicks e impressions

--- resources/views/informe_facebook.blade.php
+++ resources/views/informe_facebook.blade.php
@@ -68,21 +68,21 @@
     </style>
 </head>
 <body>
-    <!-- Página 1 -->
+    <!-- Caratula -->
     <div class="page-1" >
         <!-- Contenido de la primera página aquí -->
         <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
     </div>
     <div class="page-break"></div>
 
-    <!-- Página 2 -->
+    <!-- Logo Facebook -->
     <div class="page-2">
         <!-- Contenido de la segunda página aquí -->
         <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>        
     </div>
     <div class="page-break"></div>
 
-    <!-- Página 3 -->
+    <!-- Total Publicaciones -->
     <div class="page-3">
         <!-- Contenido de la tercera página aquí -->
         <div style="color: rgb(0, 0, 0) !important; text-align:center ; position:absolute; top:100px; left:500px; font-size:21px;"> <h1 style="color:white;font-size: 100px;">{{$datos['allPost']}} PUBLICACIONES</h1></div>
@@ -95,7 +95,7 @@
     </div>
     <div class="page-break"></div>
 
-    <!-- Páginas restantes -->
+    <!-- Interacciones -->
     <div class="page-4">
         <!-- Contenido para las siguientes páginas -->
         <div style="color: rgb(0, 0, 0) !important; text-align:center ; position:absolute; top:180px; left:350px; font-size:21px;"> <img src="{{$chartUrl}}"></div>
@@ -104,39 +104,40 @@
         <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>        
     </div>
 
-    <!-- Páginas restantes -->
+    <!-- Seguidores -->
     <div class="page-4">
         <!-- Contenido para las siguientes páginas -->
         {{--<div style="color: rgb(0, 0, 0) !important; text-align:center ; position:absolute; top:120px; left:180px; font-size:21px;"> <p>{{$fecha_inicio}} - {{$fecha_fin}}</p></div>--}}
         <div style="color: rgb(0, 0, 0) !important; text-align:center ; position:absolute; top:220px; left:180px; font-size:21px;"> <img src="{{$chartUrlSeguidores}}"></div>
         <div style="color: rgb(0, 0, 0) !important; text-align:center ; position:absolute; top:200px; left:1400px; font-size:21px;"> <h1 style="color:blue;font-size: 50px;">{{$total_seguidores}}</h1></div>
-        <div style="color: rgb(0, 0, 0) !important; text-align:center ; position:absolute; top:240px; left:1300px; font-size:21px;"> <h1 style="color:pink;font-size: 50px;">Total de Seguidores</h1></div>
+        <div style="color: rgb(0, 0, 0) !important; text-align:center ; position:absolute; top:240px; left:1250px; font-size:21px;"> <h1 style="color:pink;font-size: 50px;">Total de Seguidores</h1></div>
         <div style="color: rgb(0, 0, 0) !important; text-align:center ; position:absolute; top:300px; left:1400px; font-size:21px;"> <h1 style="color:blue;font-size: 50px;">{{$nuevos_seguidores}}</h1></div>
-        <div style="color: rgb(0, 0, 0) !important; text-align:center ; position:absolute; top:340px; left:1300px; font-size:21px;"> <h1 style="color:pink;font-size: 50px;">Nuevos Seguidores</h1></div>
+        <div style="color: rgb(0, 0, 0) !important; text-align:center ; position:absolute; top:340px; left:1250px; font-size:21px;"> <h1 style="color:pink;font-size: 50px;">Nuevos Seguidores</h1></div>
         <div style="color: rgb(0, 0, 0) !important; text-align:center ; position:absolute; top:400px; left:1380px; font-size:21px;"> <h1 style="color:red;font-size: 50px;">{{$unfollows}}</h1></div>
-        <div style="color: rgb(0, 0, 0) !important; text-align:center ; position:absolute; top:440px; left:1300px; font-size:21px;"> <h1 style="color:pink;font-size: 50px;">Unfollows</h1></div>
+        <div style="color: rgb(0, 0, 0) !important; text-align:center ; position:absolute; top:440px; left:1250px; font-size:21px;"> <h1 style="color:pink;font-size: 50px;">Seguidores Perdidos</h1></div>
         <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>        
     </div>
 
+    <!--Contenido mayor alcance-->
     <div class="page-5">
         <!--primer post-->
-        <div style="color: rgb(0, 0, 0) !important; text-align:center; position:absolute; top:420px; left:50px; font-size:21px; width:300px; height:220px; overflow:hidden; border:1px solid #ccc; padding:10px; box-sizing:border-box;"><img src="{{$imageSrcMayorAlcance1}}" style="max-width:100%; max-height:100%;" alt="Image"></div>
-        <div style="color: rgb(0, 0, 0) !important; text-align:center; position:absolute; top:180px; left:50px; font-size:21px; width:300px; padding:10px; box-sizing:border-box;">Tipo de Post : {{$datos['TopPost'][0]['type_post']}}</div>
-        <div style="color: rgb(0, 0, 0) !important; text-align:center; position:absolute; top:240px; left:50px; font-size:21px; width:300px; padding:10px; box-sizing:border-box;">Fecha del Post : {{$datos['TopPost'][0]['created_time']}}</div>
-        <div style="color: rgb(0, 0, 0) !important; text-align:center; position:absolute; top:300px; left:50px; font-size:21px; width:300px; padding:10px; box-sizing:border-box;">Tipo de publicación : {{$datos['TopPost'][0]['story']}}</div>
-        <div style="color: rgb(0, 0, 0) !important; text-align:center; position:absolute; top:180px; left:280px; font-size:21px; width:300px; padding:10px; box-sizing:border-box;">{{$sumatotalinteraccionespost1}}</div>
-        <div style="color: rgb(0, 0, 0) !important; text-align:center; position:absolute; top:200px; left:400px; font-size:21px; width:300px; padding:10px; box-sizing:border-box;">Interacciones de la publicacion</div>
-        <div style="color: rgb(0, 0, 0) !important; text-align:center; position:absolute; top:240px; left:280px; font-size:21px; width:300px; padding:10px; box-sizing:border-box;">{{$datos['TopPost'][0]['total_reacciones']}}</div>
-        <div style="color: rgb(0, 0, 0) !important; text-align:center; position:absolute; top:260px; left:320px; font-size:21px; width:300px; padding:10px; box-sizing:border-box;">Reacciones</div>
-        <div style="color: rgb(0, 0, 0) !important; text-align:center; position:absolute; top:300px; left:280px; font-size:21px; width:300px; padding:10px; box-sizing:border-box;">{{$datos['TopPost'][0]['comments_count']}}</div>
-        <div style="color: rgb(0, 0, 0) !important; text-align:center; position:absolute; top:320px; left:328px; font-size:21px; width:300px; padding:10px; box-sizing:border-box;">Comentarios</div>
-        <div style="color: rgb(0, 0, 0) !important; text-align:center; position:absolute; top:360px; left:280px; font-size:21px; width:300px; padding:10px; box-sizing:border-box;">{{$datos['TopPost'][0]['share_count']}}</div>
-        <div style="color: rgb(0, 0, 0) !important; text-align:center; position:absolute; top:380px; left:358px; font-size:21px; width:300px; padding:10px; box-sizing:border-box;">Veces Compartidas</div>
+        <div style="color: rgb(0, 0, 0) !important; text-align:center; position:absolute; top:140px; left:150px; font-size:21px; width:450px; height:270px; overflow:hidden; border:1px solid #ccc; padding:10px; box-sizing:border-box;"><img src="{{$imageSrcMayorAlcance1}}" style="max-width:100%; max-height:100%;" alt="Image"></div>
+        <div style="color: rgb(0, 0, 0) !important; text-align:center; position:absolute; top:430px; left:10px; font-size:21px; width:300px; padding:10px; box-sizing:border-box;">Tipo de Post : {{$datos['TopPost'][0]['type_post']}}</div>
+        <div style="color: rgb(0, 0, 0) !important; text-align:center; position:absolute; top:460px; left:10px; font-size:21px; width:300px; padding:10px; box-sizing:border-box;">Fecha del Post : {{$datos['TopPost'][0]['created_time']}}</div>
+        <div style="color: rgb(0, 0, 0) !important; text-align:center; position:absolute; top:520px; left:10px; font-size:21px; width:300px; padding:10px; box-sizing:border-box;">Tipo de publicación : {{$datos['TopPost'][0]['story']}}</div>
+        <div style="color: rgb(0, 0, 0) !important; text-align:center; position:absolute; top:430px; left:197px; font-size:21px; width:300px; padding:10px; box-sizing:border-box;">{{$sumatotalinteraccionespost1}}</div>
+        <div style="color: rgb(0, 0, 0) !important; text-align:center; position:absolute; top:450px; left:310px; font-size:21px; width:300px; padding:10px; box-sizing:border-box;">Interacciones de la publicacion</div>
+        <div style="color: rgb(0, 0, 0) !important; text-align:center; position:absolute; top:490px; left:197px; font-size:21px; width:300px; padding:10px; box-sizing:border-box;">{{$datos['TopPost'][0]['total_reacciones']}}</div>
+        <div style="color: rgb(0, 0, 0) !important; text-align:center; position:absolute; top:510px; left:227px; font-size:21px; width:300px; padding:10px; box-sizing:border-box;">Reacciones</div>
+        <div style="color: rgb(0, 0, 0) !important; text-align:center; position:absolute; top:550px; left:189px; font-size:21px; width:300px; padding:10px; box-sizing:border-box;">{{$datos['TopPost'][0]['comments_count']}}</div>
+        <div style="color: rgb(0, 0, 0) !important; text-align:center; position:absolute; top:570px; left:233px; font-size:21px; width:300px; padding:10px; box-sizing:border-box;">Comentarios</div>
+        <div style="color: rgb(0, 0, 0) !important; text-align:center; position:absolute; top:610px; left:195px; font-size:21px; width:300px; padding:10px; box-sizing:border-box;">{{$datos['TopPost'][0]['share_count']}}</div>
+        <div style="color: rgb(0, 0, 0) !important; text-align:center; position:absolute; top:630px; left:260px; font-size:21px; width:300px; padding:10px; box-sizing:border-box;">Veces Compartidas</div>
         <!--segundo post-->
-        <div style="color: rgb(0, 0, 0) !important; text-align:center; position:absolute; top:600px; left:900px; font-size:21px; width:300px; height:220px; overflow:hidden; border:1px solid #ccc; padding:10px; box-sizing:border-box;"><img src="{{$imageSrcMayorAlcance2}}" style="max-width:100%; max-height:100%;" alt="Image"></div>
-        <div style="color: rgb(0, 0, 0) !important; text-align:center; position:absolute; top:430px; left:900px; font-size:21px; width:300px; padding:10px; box-sizing:border-box;">Tipo de Post : {{$datos['TopPost'][1]['type_post']}}</div>
-        <div style="color: rgb(0, 0, 0) !important; text-align:center; position:absolute; top:460px; left:900px; font-size:21px; width:300px; padding:10px; box-sizing:border-box;">Fecha del Post : {{$datos['TopPost'][1]['created_time']}}</div>
-        <div style="color: rgb(0, 0, 0) !important; text-align:center; position:absolute; top:520px; left:900px; font-size:21px; width:300px; padding:10px; box-sizing:border-box;">Tipo de publicación : {{$datos['TopPost'][1]['story']}}</div>
+        <div style="color: rgb(0, 0, 0) !important; text-align:center; position:absolute; top:140px; left:900px; font-size:21px; width:450px; height:270px; overflow:hidden; border:1px solid #ccc; padding:10px; box-sizing:border-box;"><img src="{{$imageSrcMayorAlcance2}}" style="max-width:100%; max-height:100%;" alt="Image"></div>
+        <div style="color: rgb(0, 0, 0) !important; text-align:center; position:absolute; top:430px; left:800px; font-size:21px; width:300px; padding:10px; box-sizing:border-box;">Tipo de Post : {{$datos['TopPost'][1]['type_post']}}</div>
+        <div style="color: rgb(0, 0, 0) !important; text-align:center; position:absolute; top:460px; left:800px; font-size:21px; width:300px; padding:10px; box-sizing:border-box;">Fecha del Post : {{$datos['TopPost'][1]['created_time']}}</div>
+        <div style="color: rgb(0, 0, 0) !important; text-align:center; position:absolute; top:520px; left:800px; font-size:21px; width:300px; padding:10px; box-sizing:border-box;">Tipo de publicación : {{$datos['TopPost'][1]['story']}}</div>
         <div style="color: rgb(0, 0, 0) !important; text-align:center; position:absolute; top:430px; left:1180px; font-size:21px; width:300px; padding:10px; box-sizing:border-box;">{{$sumatotalinteraccionespost2}}</div>
         <div style="color: rgb(0, 0, 0) !important; text-align:center; position:absolute; top:450px; left:1300px; font-size:21px; width:300px; padding:10px; box-sizing:border-box;">Interacciones de la publicacion</div>
         <div style="color: rgb(0, 0, 0) !important; text-align:center; position:absolute; top:490px; left:1180px; font-size:21px; width:300px; padding:10px; box-sizing:border-box;">{{$datos['TopPost'][1]['total_reacciones']}}</div>
@@ -148,9 +149,10 @@
         <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>        
     </div>
 
+    <!--contenido mas compartido-->
     <div class="page-6">
-        <div style="position:relative; left:600px;">
-            <div style="color: rgb(0, 0, 0) !important; text-align:center; position:absolute; top:420px; left:50px; font-size:21px; width:300px; height:220px; overflow:hidden; border:1px solid #ccc; padding:10px; box-sizing:border-box;"><img src="{{$imageSrcCompartido}}" style="max-width:100%; max-height:100%;" alt="Image"></div>
+        <div style="position:relative; left:600px;border:1px solid #ccc;">
+            <div style="color: rgb(0, 0, 0) !important; text-align:center; position:absolute; top:140px; left:-600px; font-size:21px; width:630px; height:470px; overflow:hidden; border:1px solid #ccc; padding:10px; box-sizing:border-box;"><img src="{{$imageSrcCompartido}}" style="max-width:100%; max-height:100%;" alt="Image"></div>
             <div style="color: rgb(0, 0, 0) !important; text-align:center; position:absolute; top:180px; left:50px; font-size:21px; width:300px; padding:10px; box-sizing:border-box;">Tipo de Post : {{$datos['getMostSharedPost']['type_post']}}</div>
             <div style="color: rgb(0, 0, 0) !important; text-align:center; position:absolute; top:240px; left:50px; font-size:21px; width:300px; padding:10px; box-sizing:border-box;">Fecha del Post : {{$datos['getMostSharedPost']['created_time']}}</div>
             <div style="color: rgb(0, 0, 0) !important; text-align:center; position:absolute; top:300px; left:50px; font-size:21px; width:300px; padding:10px; box-sizing:border-box;">Tipo de publicación : {{$datos['getMostSharedPost']['story']}}</div>
@@ -166,9 +168,10 @@
         <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>        
     </div>
 
+    <!--contenido mas comentado-->
     <div class="page-7">
         <div style="position:relative; left:600px;">
-            <div style="color: rgb(0, 0, 0) !important; text-align:center; position:absolute; top:420px; left:50px; font-size:21px; width:300px; height:220px; overflow:hidden; border:1px solid #ccc; padding:10px; box-sizing:border-box;"><img src="{{$imageSrcComentario}}" style="max-width:100%; max-height:100%;" alt="Image"></div>
+            <div style="color: rgb(0, 0, 0) !important; text-align:center; position:absolute; top:140px; left:-600px; font-size:21px; width:630px; height:470px; overflow:hidden; border:1px solid #ccc; padding:10px; box-sizing:border-box;"><img src="{{$imageSrcComentario}}" style="max-width:100%; max-height:100%;" alt="Image"></div>
             <div style="color: rgb(0, 0, 0) !important; text-align:center; position:absolute; top:180px; left:50px; font-size:21px; width:300px; padding:10px; box-sizing:border-box;">Tipo de Post : {{$datos['getMostCommentsPost']['type_post']}}</div>
             <div style="color: rgb(0, 0, 0) !important; text-align:center; position:absolute; top:240px; left:50px; font-size:21px; width:300px; padding:10px; box-sizing:border-box;">Fecha del Post : {{$datos['getMostCommentsPost']['created_time']}}</div>
             <div style="color: rgb(0, 0, 0) !important; text-align:center; position:absolute; top:300px; left:50px; font-size:21px; width:300px; padding:10px; box-sizing:border-box;">Tipo de publicación : {{$datos['getMostCommentsPost']['story']}}</div>
@@ -184,10 +187,7 @@
         <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>        
     </div>
 
-    {{--<div class="page-8">
-        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>        
-    </div>--}}
-
+    <!--Imagen de Gracias-->
     <div class="page-9">
         <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>        
     </div>
