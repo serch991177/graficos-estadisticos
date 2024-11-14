647aa90484fd29ca78a7f589b2ed4e0c75938b0a
SERGIO ANDIA <sergioandia11@gmail.com>
2024-11-14 14:58:01 -0400

    agregado del modal

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1233,6 +1233,41 @@
   </div>
 </div>
 <!--Fin Modal Reacciones-->
+<!--Modal PDF-->
+<div class="modal fade" id="PdfModal" tabindex="-1" role="dialog" aria-labelledby="PdfModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title col-11 text-center" id="PdfModalLabel"></h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+        <div class="modal-body">
+            <h1 class="text-center">Generacion de Informe</h1>
+            <form action="{{ route('informe_id_escucha') }}" method="post" target="_blank">
+                @csrf 
+                <input type="hidden" name="id" id="id_generar_pdf" value="">
+                <div class="row">
+                    <div class="col-md-12 text-center">
+                        <label>Ingrese el contexto</label>
+                        <textarea required name="contexto" id="contexto" cols="35" rows="4"></textarea>
+                    </div>
+                </div>
+                <div class="text-center">
+                    <button class="btn btn-warning" title="Generar PDF"><i class="fas fa-file-pdf"></i></button>
+                </div>
+            </form>
+        </div>
+      </div> 
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+        <!--<button type="button" class="btn btn-primary">Save changes</button>-->
+      </div>
+    </div>
+  </div>
+</div>
+<!--Fin modal pdf-->
 <!--Bar Race-->
 <!--Scripts de ciudades-->
 <!--Scripts de javascript-->
@@ -1316,12 +1351,15 @@
                             <i class="fas fa-chart-bar"></i>
                         </button>
                         <br><br>
-                        <form action="{{ route('informe_id_escucha') }}" method="post" target="_blank">
+                        <button type="button" title="Generar PDF" class="btn btn-warning id_pdf" value="${row.id}" data-toggle="modal" data-target="#PdfModal">
+                            <i class="fas fa-file-pdf"></i>
+                        </button>
+                        {{--<form action="{{ route('informe_id_escucha') }}" method="post" target="_blank">
                             @csrf 
                             <input type="hidden" name="id" value="${row.id}">
                             <input type="hidden" name="contexto" value="">
                             <button class="btn btn-warning" title="Generar PDF"><i class="fas fa-file-pdf"></i></button>
-                        </form>
+                        </form>--}}
                     `;
                 }
             }
@@ -1462,11 +1500,14 @@
                             <i class="fas fa-chart-bar"></i>
                         </button>
                         <br><br>
-                        <form action="{{ route('informe_id_escucha') }}" method="post" target="_blank">
+                        <button type="button" title="Generar PDF" class="btn btn-warning id_pdf" value="${row.id}" data-toggle="modal" data-target="#PdfModal">
+                            <i class="fas fa-file-pdf"></i>
+                        </button>
+                        {{--<form action="{{ route('informe_id_escucha') }}" method="post" target="_blank">
                             @csrf 
                             <input type="hidden" name="id" value="${row.id}">
                             <button class="btn btn-warning" title="Generar PDF"><i class="fas fa-file-pdf"></i></button>
-                        </form>
+                        </form>--}}
                     `;
                 }
             }
@@ -2117,6 +2158,16 @@
                     $('#spinner').hide();
                 }
             });
+        });
+    });
+</script>
+<!--Funcion para recuperar el id del pdf-->
+<script>
+    $(document).ready(function(){
+        $(document).on('click','.id_pdf',function(){
+            var id = $(this).val();
+            document.getElementById("contexto").value = "";
+            document.getElementById("id_generar_pdf").value = id;
         });
     });
 </script>
